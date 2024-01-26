c32845ec9fc0154406451ddd72ee07ecaed31a32
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-01-26 16:01:23 +0100

    update

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  let newArray = firstArray.concat(secondArray);
+
+  for (let i = 0; i < newArray.length; i += 1) {
+    if (newArray.length > maxLength) {
+      return newArray.slice(0, maxLength);
+    } else {
+      return newArray;
+    }
+  }
+}
+
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
