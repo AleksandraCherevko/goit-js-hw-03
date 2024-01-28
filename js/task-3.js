780b804f0f7de255f4d233571c5ea3d7a80718c9
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-01-28 20:29:59 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,12 @@
-function filterArray(numbers, value) {
-  let newArray = [];
-  for (let number of numbers) {
-       if (value )
-  }
-}
+// function filterArray(numbers, value) {
+//   let newArray = [];
+//   for (let number of numbers) {
+//        if (value )
+//   }
+// }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
