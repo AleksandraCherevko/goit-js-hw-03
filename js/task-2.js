eedc891bdded0a951caaa0e34aa92b94380b73f9
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-02-07 19:22:59 +0100

    update

--- js/task-2.js
+++ js/task-2.js
@@ -1,27 +1,11 @@
 function makeArray(firstArray, secondArray, maxLength) {
   let newArray = firstArray.concat(secondArray);
-
-  for (let i = 0; i < newArray.length; i += 1) {
-    return newArray.slice(0, maxLength);
-  }
-  return newArray;
+  return newArray.slice(0, maxLength);
 }
 
-console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
-
-// function makeArray(firstArray, secondArray, maxLength) {
-//   let newArray = firstArray.concat(secondArray);
-
-//   for (let i = 0; i < newArray.length; i += 1) {
-//     if (newArray.length > maxLength) {
-//       return newArray.slice(0, maxLength);
-//     } else {
-//       return newArray;
-//     }
-//   }
-// }
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3));
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4));
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0));
